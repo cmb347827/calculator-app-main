9c1257207e86efcebd695b71a27c269fc2b4bedc
cmb347827 <v38d422ll621dem@gmail.com>
2024-11-18 15:59:03 -0500

    checkoperands changes

--- calculator-app-main.js
+++ calculator-app-main.js
@@ -4,14 +4,24 @@ let numOperandsArr=[];
 
 
 const checkOperands=()=>{
+    console.log('1st ',numOperandsArr);
+    let firstOp = false;
     const tempArr= numOperandsArr.slice(numOperandsArr.length-2);
-    //use ascii codes with includes for operands: / + - x
-    const operands=['&#x2b','&#x2212','&#8725','&#x2716'];
-    tempArr.forEach((operand)=>{
-        console.log('in check',tempArr,' ',operand);
-       if(operands.includes(operand)){
-          console.log('yes');
-       }
+    //use hex codes for operands: / + - x ,  ex: &#x2215; (hex for / is 2215 in the operands array)
+    const allOperands=['2b','2212','2215','2716'];
+    tempArr.forEach((numOrOperand)=>{
+        //convert calculator operand to it's hexadecimal value (to ensure if it's different = + etc all are passable)
+        const hex =numOrOperand.charCodeAt(0).toString(16);
+        if(firstOp){
+            //tempArr contains two operands one after the other.
+            //Remove the pre-last operand from numOperandsArr, so only the last operand added is applied.
+            numOperandsArr.splice(numOperandsArr.length-2,1);
+            console.log('2de', numOperandsArr);
+        }
+        if(allOperands.includes(hex)){
+            //one is an operand set firstOp to true
+             firstOp=true;
+        }
     });
 };
 
