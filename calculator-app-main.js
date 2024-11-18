da270bb3f5ecdceb90fe97d7e1b6bfaf7894dfde
cmb347827 <v38d422ll621dem@gmail.com>
2024-11-17 21:24:40 -0500

    addeventlisteners

--- calculator-app-main.js
+++ calculator-app-main.js
@@ -1,11 +1,42 @@
 'use strict'; 
 
 
+const calcAnswer=()=>{
+    console.log('in calc');
+};
+const deleteNum=()=>{
+   console.log('in dele');
+};
+const reset=()=>{
+   console.log('in reset');
+};
+
+const buttonListeners=()=>{
+    document.querySelectorAll('.calc-button').forEach((btn)=>{
+       btn.addEventListener('click',()=>{
+           if(btn.textContent==='=' || btn.textContent==='reset' || btn.textContent==='del'){
+                 const value= btn.textContent;
+
+                 switch(value){
+                      case '=':
+                           calcAnswer();
+                           break;
+                      case 'del':
+                           deleteNum();
+                           break;
+                      case 'reset':
+                           reset();
+                           break;
+                 }
+           }
+       });
+    });
+};
 
 
 
 addEventListener("load", (event) => {
-   
+     buttonListeners();
 
 });
 
