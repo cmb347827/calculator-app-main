4a46286127d43830ba70180fa06d56db4400ef54
cmb347827 <v38d422ll621dem@gmail.com>
2024-11-22 13:04:16 -0500

    made some changes, remove two functions, replaced with a new function

--- calculator-app-main.js
+++ calculator-app-main.js
@@ -54,39 +54,38 @@ const checkMinus=()=>{
               break;
        }
     } else if(numOperandsArr.length===2){
-            //either for instance :  number-
-            // +- or -- are allowed in multiple operands (with a default zero or entered number)
-            // x- and /- are allowed as well
+            //either for instance :  number- , -number
+            
+            
             const charArr = numOperandsArr.slice(numOperandsArr.length-2);
             const bothOperands= charArr.every(testOperands);
-            //if bothOperands, test second operand. if minus, leave operands as is , return 0 with operands
+        
             const lastChar = convertHexOperand(numOperandsArr[numOperandsArr.length-1]);
-            if(bothOperands && !isMinus){
-                if(lastChar==='2212'){
+            if(bothOperands){
+                //due to !isMinus , first operand is not a minus.
+                //if bothOperands, test second operand. if minus, leave operands as is , return 0 with operands
+                if(lastChar==='2212' && !isMinus){
+                    // +- are allowed in multiple operands (with a default zero or entered number)
+                    // x- and /- are allowed as well
                     numOperandsArr.unshift('0');
                     output.textContent=numOperandsArr.join('');
+                }else{
+                     //++ , -+ , etc not allowed , and -- not allowed if no number entered yet.
+                     //splice the second operand, so ++ is just +
+                    numOperandsArr=numOperandsArr.splice(numOperandsArr.length-1,1);
                 }
             }
-            else if(lastChar==='2212' && isMinus){
-                //if oneMinus=== true, it means the user is trying to enter two minuses before the first operator,just: -- number.
-                //splice the second minus, so --number is just -number.
-                numOperandsArr=numOperandsArr.splice(numOperandsArr.length-1,1);
-            }
-            //second operand is not minus , remove second operand , return 0 with first operand.
-            else if(lastChar!=='2212'){
-                numOperandsArr=numOperandsArr.splice(numOperandsArr.length-1,1);
-                numOperandsArr.unshift('0');
+            else {
                 output.textContent=numOperandsArr.join('');
             }
-            //if only one operand,  return numOperandsArr.
-            else if(!bothOperands){
-                return numOperandsArr.join('');
-            }else{
-                return numOperandsArr.join('');
-            }
+            
 
-    } else if(numOperandsArr.length > 2){
-        return numOperandsArr.join('');
+    } else if(numOperandsArr.length ===3){
+        isMinus=false;
+        //could be -33 , 3-3, 3-- etc
+
+    } else if(numOperandsArr.length>3){
+
     }
 };
 
@@ -104,12 +103,12 @@ const reset=()=>{
 const buttonListeners=()=>{
     document.querySelectorAll('.calc-button').forEach((btn)=>{
        btn.addEventListener('click',()=>{
-           isMinus=false;
+           //isMinus=false;
            //add pressed button value to numOperandsArr[]
            numOperandsArr.push(btn.textContent);
            //check to see if an operand was entered first or a number
            checkMinus();
-
+           output.textContent=numOperandsArr.join('');
            if(btn.textContent==='=' || btn.textContent==='reset' || btn.textContent==='del'){
                  const value= btn.textContent;
 
